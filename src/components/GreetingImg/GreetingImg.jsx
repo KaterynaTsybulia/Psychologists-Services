9b856cc0d3e61aa9ed7b5fcbbd6269a9e587714b
Katerina <ekaterina041117@gmail.com>
2025-03-29 20:49:57 +0200

    homeImg

--- src/components/GreetingImg/GreetingImg.jsx
+++ src/components/GreetingImg/GreetingImg.jsx
@@ -1,12 +1,15 @@
-import GreetingExperience from "../GreetingExperience/GreetingExperience";
 import css from "./GreetingImg.module.css";
+import GreetingExperience from "../GreetingExperience/GreetingExperience";
+import homeImage1x from "../../assets/image/home@1x.jpg";
+import homeImage2x from "../../assets/image/home@2x.jpg";
 
 const GreetingImg = () => {
 	return (
 		<li className={css.liImg}>
 			<img
 				className={css.img}
-				src="/src/assets/image/home@2x.jpg"
+				src={homeImage1x}
+				srcSet={`${homeImage1x} 1x, ${homeImage2x} 2x`}
 				alt="Girl with glasses"
 			/>
 			<div className={css.greetingExperienceContainer}>
